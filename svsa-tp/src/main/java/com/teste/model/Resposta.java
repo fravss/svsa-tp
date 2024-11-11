1f5a2c97d628d5fd3917a63ef3ed6ec10898a054
Gadu05 <106440419+Gadu05@users.noreply.github.com>
2024-11-11 08:29:48 -0300

    first commit

---
+++ svsa-tp/src/main/java/com/teste/model/Resposta.java
@@ -0,0 +1,45 @@
+package com.teste.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name="usuario")
+public class Resposta implements Serializable {
+
+	private static final long serialVersionUID = 82375949344894033L;
+
+	@Id
+	private Long codigo;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCriacao;	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataModificacao;
+
+	@PrePersist
+	@PreUpdate
+	public void configuraDatasCriacaoAlteracao() {
+		this.setDataModificacao( new Date() );
+				
+		if (this.getDataCriacao() == null) {
+			this.setDataCriacao( new Date() );
+		}		
+	}
+}
---
+++ svsa-tp/src/main/java/com/teste/model/enums/StatusOcorrencia.java
@@ -0,0 +1,10 @@
+package com.teste.model.enums;
+
+public enum StatusOcorrencia {
+	
+	ABERTO,
+	COORDENADOR,
+	GESTOR,
+	FECHADO
+	
+}
